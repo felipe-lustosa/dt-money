1cd07359f178921be0f83276b8d0ab5d0f2713ee
Felipe Lustosa <felipelustosaa6@gmail.com>
2023-05-12 12:13:15 -0300

    json server and summary data

--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -34,5 +34,5 @@ interface PriceHighLightProps {
 }
 
 export const PriceHighLight = styled.span<PriceHighLightProps>`
-  color: ${props => props.variant == 'income' ? props.theme['green-300'] : props.theme['red-300']}
+  color: ${props => props.variant == 'income' ? props.theme['green-300'] : props.theme['red-300']};
 `
