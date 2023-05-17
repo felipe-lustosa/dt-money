ff4b195aa58e253d3c4085ca0fb8755ae3eca1c4
Felipe Lustosa <felipelustosaa6@gmail.com>
2023-05-17 14:46:48 -0300

    fixing error in search form component

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -12,7 +12,7 @@ const searchFormSchema = z.object({
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
-export function SearchFormComponent() {
+export function SearchForm() {
   const fetchTransactions = useContextSelector(
     TransactionsContext,
     (context) => {
